c754dd085d1eb535102ec0cf94983f27122df446
luiz <luiz.carvalho.097@gmail.com>
2025-07-04 10:18:42 -0300

    def de pixel e cores

---
+++ paleta.h
@@ -0,0 +1,13 @@
+class Pixel {
+    public:
+    unsigned char R;
+    unsigned char G;
+    unsigned char B;
+};
+
+class Cores {
+    int quantidade;
+    Pixel cores[100]; 
+    double valores[100];
+};
+
