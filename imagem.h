6f3153ca6de00da19fb59b3a41f75593dc9183de
Nalanda-ux <eduardanalanda872@gmail.com>
2025-07-17 08:36:08 -0300

    Update imagem.h

--- imagem.h
+++ imagem.h
@@ -1,14 +1,15 @@
 #ifndef IMAGEM_H
 #define IMAGEM_H
 #include "paleta.h"
-namespace img {
+namespace img { // criando pra usar no .cpp
     class Imagem {
 private:
         int largura;
         int altura;
-        pc::Cor* pixels; //pra ser alocado dinamicamente o array de pixels
+        pc::Cor* pixels; //ponteiro para o primeiro elemento de um array dinamico, que vai ser repleto de elementos do tipo Cor
+        //pra ser alocado dinamicamente o array de pixels
 public:
-        Imagem(int largura, int altura); //cria img com alt e larg especificadas
+        Imagem(int largura, int altura); // construtor para cria imagem com alt e larg especificadas
         ~Imagem(); //destrutor para liberar a memoria
         void DefPix(int x, int y, pc::Cor cor); //def a cor de um pixel em determinadas coordenadas
         pc::Cor obtPix(int x, int y) const; //obtem a cor de um pixel em deteminadas coordenadas
